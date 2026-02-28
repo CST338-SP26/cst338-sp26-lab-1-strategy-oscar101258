09ef9b4e182534f825324c69023c44d3c8169b29
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-02-28 12:42:35 -0800

    complete monster class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
  * 08/27/25
  * 0.1.0
  */
-public class Monster {
+public abstract class Monster {
     private Integer hp;
     private Integer xp;
     private Integer maxHP;
@@ -16,5 +16,49 @@ public class Monster {
         hp = this.maxHP;
         this.xp = xp;
         this.items = items;
+    }
+
+
+    public boolean equals(Object o) {
+        return true;
+    }
+    public int hashCode() {
+        return 0;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getXp() {
+        return xp;
+    }
+
+    public void setXp(Integer xp) {
+        this.xp = xp;
+    }
+
+    public Integer getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(Integer maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public HashMap<String, Integer> getItems() {
+        return items;
+    }
+
+    public void setItems(HashMap<String, Integer> items) {
+        this.items = items;
+    }
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "has : hp=" + hp + "/" + maxHP;
     }
 }
