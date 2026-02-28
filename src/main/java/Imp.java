fcd070ba238d21a70dd3f4d1501d4854798001a2
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-02-28 13:03:35 -0800

    add imp and kobold classes

---
+++ src/main/java/Imp.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+/**
+ *
+ * @author Oscar
+ * 08/27/25
+ * 0.1.0
+ */
+public class Imp extends Monster {
+    public Imp(Integer maxHP, Integer xp, HashMap<String, Integer>items){
+        super(maxHP, xp, items);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/Kobold.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+/**
+ *
+ * @author Oscar
+ * 08/27/25
+ * 0.1.0
+ */
+public class Kobold extends Monster {
+    public Kobold(Integer maxHP, Integer xp, HashMap<String, Integer> items){
+        super(maxHP, xp, items);
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
