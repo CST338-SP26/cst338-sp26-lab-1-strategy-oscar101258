fd9bdd032fe5b9d542046f923fe633f83b10152a
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-03-01 08:30:22 -0800

    completed driver class

---
+++ src/main/java/Driver.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author Oscar
+ * 08/27/25
+ * 0.1.0
+ */
+public class Driver {
+    public static void main(String[] args) {
+        HashMap<String, Integer> items = new HashMap<>();
+        items.put("gold", 5);
+        List<Monster> monsters = new ArrayList<>();
+        monsters.add(new Imp(15, 20, items));
+        monsters.add(new Kobold(1, 5, items));
+
+        for(Monster m : monsters){
+            System.out.println(m);
+        }
+    }
+}
